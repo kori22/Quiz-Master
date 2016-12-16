42e2e03bced728f540ce0fc7cb068723f0c6c067
Bukhari Mardius <mardius.b@gmail.com>
2016-12-16 07:32:56 +0700

    fix migrate

--- db/migrate/20161214130916_add_time_to_subject.rb
+++ db/migrate/20161214130916_add_time_to_subject.rb
@@ -1,6 +1,6 @@
 class AddTimeToSubject < ActiveRecord::Migration[5.0]
   def change
   	add_column :subjects, :duration, :integer, default: 60
-  	# add_column :subjects, :tot_question, :integer, default: 5
+  	add_column :subjects, :tot_question, :integer, default: 5
   end
 end
